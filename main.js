7b6c514ecb1c5d7e5d9742acbb032ebc1f9c47a7
seafood <mail.seafood@proton.me>
2024-03-16 21:29:31 +0300

    Add Electron main window and event handlers

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log("hello world!");
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+const isMac = process.platform === 'darwin';
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: 'Image Resizer',
+    width: 500,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
