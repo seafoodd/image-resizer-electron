5078435a393cc38416061add7b18a6a8f765fb92
seafood <mail.seafood@proton.me>
2024-03-16 21:44:18 +0300

    Update main window width and open devtools in dev environment

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
+const isDev = process.env.NODE_ENV !== 'development';
 const isMac = process.platform === 'darwin';
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'Image Resizer',
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   });
+
+  // Open devtools if in dev environment
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
